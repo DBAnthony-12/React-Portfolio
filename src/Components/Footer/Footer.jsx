2f69b83589403e7daba1fa64051eed9d4827d20e
DBAnthony-12 <126782224+DBAnthony-12@users.noreply.github.com>
2024-06-26 12:19:26 +0200

    publish to github

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,34 +1,59 @@
 import React from "react";
-import './Footer.css'
-import logo from '../../assets/nameLogo.png'
-import user from '../../assets/user_icon.png'
-
+import "./Footer.css";
+import logo from "../../assets/nameLogo.png";
+import user from "../../assets/user_icon.png";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = () => {
   return (
     <div className="footer">
-        <div className="footer-top">
-            <div className="footer-top-left">
-                {/* <img src={logo} alt=""/> */}
-                {/* <p>Insert message</p> */}
-            </div>
-            <div className="footer-top-right">
-                {/* <div className="footer-email-input">
+      <div className="footer-top">
+        <div className="footer-top-left">
+          {/* <img src={logo} alt=""/> */}
+          {/* <p>Insert message</p> */}
+        </div>
+        <div className="footer-top-right">
+          {/* <div className="footer-email-input">
                     <img src={user} alt=""/>
                     <input type="email" placeholder="Enter your email" />
                 </div> */}
-                {/* <div className="footer-subscribe">Subscribe</div> */}
-            </div>
+          {/* <div className="footer-subscribe">Subscribe</div> */}
         </div>
-        <hr/>
-        <div className="footer-bottom">
-            <p className="footer-bottom-left">2024 Dilshaad Anthony. All rights reserved</p>
-            <div className="footer-bottom-right">
-                <p>Terms of Services</p>
-                <p>Privacy Policy</p>
-                <p>Connect with me</p>
+      </div>
+      <hr />
+      <div className="footer-bottom">
+        <p className="footer-bottom-left">
+          2024 Dilshaad Anthony. All rights reserved
+        </p>
+        <div className="footer-bottom-right">
+          <p>Terms of Services</p>
+          <p>Privacy Policy</p>
+          <div className="connect-container">
+            <p>Connect with me</p>
+            <div className="social-icons">
+              <a
+                href="https://github.com/DBAnthony-12/chatApp"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="social-icon"
+                data-label="Github"
+              >
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/dilshaad-b-anthony-79455b270/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="social-icon"
+                data-label="LinkedIn"
+              >
+                <FontAwesomeIcon icon={faLinkedin} />
+              </a>
             </div>
+          </div>
         </div>
+      </div>
     </div>
   );
 };
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Hero.css";
 import me2 from "../../assets/me2.jpg";
-import resume from "../../assets/resume.pdf"
+import resume from "../../assets/resume.pdf";
 import { Link } from "react-scroll";
 
 const Hero = () => {
@@ -13,23 +13,52 @@ const Hero = () => {
     <div className="hero">
       <img src={me2} alt="Dilshaad Anthony" />
       <h1>
-        <span>Hi, I am Dilshaad Anthony.</span><br /> I am a full-Stack
-        Software Developer
+        <span>Hi, I am Dilshaad Anthony.</span>
+        <br /> I am a full-Stack Software Developer
       </h1>
       <p>
-        Hello!, I'm a web development enthusiast, dedicated to crafting responsive and user-friendly websites. With a diverse skill set in various 
-        programming languages, I bring creativity and innovation to every project. Let's build something amazing together!
+        Hello!, I'm a web development enthusiast, dedicated to crafting
+        responsive and user-friendly websites. With a diverse skill set in
+        various programming languages, I bring creativity and innovation to
+        every project. Let's build something amazing together!
       </p>
       <div className="hero-action">
         <div className="nav-connect">
-          <Link to="contact" spy={true} smooth={true} offset={5} duration={500} onClick={closeMenu}>
+          <Link
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={5}
+            duration={500}
+            onClick={closeMenu}
+          >
             Connect with me
           </Link>
         </div>
-        <a href={resume} target="_blank" rel="noopener noreferrer">
-          <div className="hero-resume">View Resume</div>
-        </a>
-        <div className="contactMe">Contact me</div>
+
+        <div className="resume-container">
+          <a
+            href={resume}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn-link"
+          >
+            <button className="btn">View Resume</button>
+          </a>
+        </div>
+
+        <div className="contactMe">
+          <Link
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={5}
+            duration={500}
+            onClick={closeMenu}
+          >
+            Contact me
+          </Link>
+        </div>
       </div>
     </div>
   );
